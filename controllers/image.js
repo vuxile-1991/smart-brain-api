58934c3198044bc98a5f8517c652ecbe637ebcb7
unknown <vuxile@gmail.com>
2020-10-20 10:06:17 +0200

    commit message

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require ('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App ({
+	apiKey: process.env.API_KEY
+});
+
+const handleApiCall = (req, res) => {
+app.models
+	.predict(process.env.API_KEY, req.body.input)
+	.then(data => {res.json(data);})
+	.catch(err => res.status(400).json('Unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+  	.increment('entries', 1)
+  	.returning('entries')
+  	.then(entries => {
+  		res.json(entries[0]);
+  	}) 
+  	.catch(err => res.status(400).json('Unable to get entries'))
+  }
+
+  module.exports = {
+	handleImage,
+	handleApiCall
+};
+
